3cd4426196306f45842c5e5296a99c9a11c94e13
Tasveer Singh <taz@tazsingh.com>
2017-01-10 19:01:17 -0500

    Get most of it working!

---
+++ devServer.js
@@ -0,0 +1,30 @@
+import { execSync } from 'child_process'
+import fs from 'fs'
+import express from 'express'
+import webpack from 'webpack'
+import config from './webpack.config'
+
+import devMiddleware from 'webpack-dev-middleware'
+
+const indexHTML = fs.readFileSync('./build/index.html')
+const app = express()
+const compiler = webpack(config)
+
+app.use(devMiddleware(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}))
+
+app.get('*', (req, res) => {
+  res.end(indexHTML)
+})
+
+app.listen(1337, () => {
+  console.log('Listening on port 1337')
+
+  const openCommand = execSync('which open', { encoding: 'utf-8' })
+
+  if (openCommand) {
+    execSync(`open http://localhost:1337`)
+  }
+})
